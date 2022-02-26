2ed773b3761172cafdcfb39be920866875353fe7
Manali Darji <mvdarji@seneca.ca>
2022-02-26 16:47:14 -0500

    implemented all properties listings functionality

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,41 +6,43 @@ import Footer from '../components/Footer';
 import { useState, useEffect } from 'react';
 
 const HomePage = () => {
-	// let [properties, setProperties] = useState([]);
-	let [bestSellerProperties, setBestSellerProperties] = useState([]);
-	let [propTypes, setPropTypes] = useState([]);
+	// let [allProperties, setProperties] = useState([]);
+	// let [bestSellerProperties, setBestSellerProperties] = useState([]);
+	// let [propTypes, setPropTypes] = useState([]);
 
-	// get data from API
-	const allPropertiesApiURL = 'http://localhost:1000/properties';
-	const propertiesTypeApiURL = 'http://localhost:1000/propertyTypes';
+	// // get data from API
+	// const APIRootURL = 'http://localhost:1000';
+	// const noOfPropTypes = 5;
+	// const allPropertiesApiURL = `${APIRootURL}/properties`;
+	// const propertiesTypeApiURL = `${APIRootURL}/propertyTypes?_limit=${noOfPropTypes}`;
 
-	useEffect(() => {
-		fetch(allPropertiesApiURL)
-		.then( resp => resp.json() )
-		.then( data => {
-			let bestSellerProps = data.filter( property => property.bestSeller);
-			setBestSellerProperties(bestSellerProps);
-			// setProperties(data);
-		})
-		.catch( error => {
-			console.error(error);
-		});
+	// useEffect(() => {
+	// 	fetch(allPropertiesApiURL)
+	// 	.then( resp => resp.json() )
+	// 	.then( data => {
+	// 		let bestSellerProps = data.filter( property => property.bestSeller);
+	// 		setBestSellerProperties(bestSellerProps);
+	// 		setProperties(data);
+	// 	})
+	// 	.catch( error => {
+	// 		console.error(error);
+	// 	});
 
-		fetch(propertiesTypeApiURL)
-		.then( resp => resp.json() )
-		.then( json => setPropTypes(json))
-		.catch( error => {
-			console.error(error);
-		});
-	}, []);
+	// 	fetch(propertiesTypeApiURL)
+	// 	.then( resp => resp.json() )
+	// 	.then( json => setPropTypes(json))
+	// 	.catch( error => {
+	// 		console.error(error);
+	// 	});
+	// }, [propertiesTypeApiURL, allPropertiesApiURL]);
 
 	return (
 		<>
 		<Header/>
-		<Hero/>
-        <PropertyTypeLists allPropTypes={propTypes}/>
-        <BestSellerLists allProperties={bestSellerProperties}/>
-        <Footer/>
+		{/* <Hero/> */}
+        {/* <PropertyTypeLists/> */}
+        {/* <BestSellerLists /> */}
+        {/* <Footer/> */}
 		</>
 	)
 }
--- src/pages/PropListingsPage.js
+++ src/pages/PropListingsPage.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import PropertyListingsItem from '../components/PropertyListingsItem';
+
 const PropListingsPage = () => {
-  return (
-    <div>PropListingsPage</div>
-  )
+	// using state for all properties lists
+	let [properties, setProperties] = useState([]);
+
+	// get all properties
+	useEffect( () => {
+		const allPropertiesApiURL = `http://localhost:1000/properties`;
+		fetch(allPropertiesApiURL)
+		.then( resp => resp.json())
+		.then( data => setProperties(data))
+		.catch( error => console.log(error));
+	}, []);
+
+	return (
+		<div>
+			{properties.map(property => (
+				<Link 
+					to={`/properties/${property.id}`} 
+					key={property.id}
+				>
+					<PropertyListingsItem 						
+						id={property.id}
+						singleProperty={property}
+					/>
+				</Link>
+			))}
+		</div>
+	)
 }
 
 export default PropListingsPage
