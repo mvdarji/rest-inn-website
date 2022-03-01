1ad6742d85d5eb7191f52c338e5ea43d5bd58a0c
Manali Darji <mvdarji@seneca.ca>
2022-03-01 18:31:42 -0500

    refactored footer icon code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,10 +27,10 @@ const Footer = () => {
                     <div className="footer-col">
                         <p className="footer-col-title">find us</p>
                         <ul className="social-links-ul">
-                            <li><Link to='/'><FaFacebookF className="fa" />Facebook</Link></li>
-                            <li><Link to='/'><FaTwitter className="fa" />Twitter</Link></li>
-                            <li><Link to='/'><FaWhatsapp className="fa" />WhatsApp</Link></li>
-                            <li><Link to='/'><FaVimeoV className="fa" />Vimeo</Link></li>
+                            <li><Link to='/' className="icon-text-wrap"><FaFacebookF className="fa" />Facebook</Link></li>
+                            <li><Link to='/' className="icon-text-wrap"><FaTwitter className="fa" />Twitter</Link></li>
+                            <li><Link to='/' className="icon-text-wrap"><FaWhatsapp className="fa" />WhatsApp</Link></li>
+                            <li><Link to='/' className="icon-text-wrap"><FaVimeoV className="fa" />Vimeo</Link></li>
                         </ul>
                     </div>
                 </div>
