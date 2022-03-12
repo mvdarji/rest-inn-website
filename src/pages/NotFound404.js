4c3a0022b5a743fd8907b21507b224e70cb81af5
Manali Darji <mvdarji@seneca.ca>
2022-03-11 21:22:17 -0500

    404 page: implementation done

--- src/pages/NotFound404.js
+++ src/pages/NotFound404.js
@@ -1,7 +1,27 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import img404 from '../assets/images/404.gif';
+
 const NotFound404 = () => {
-    return(
-        <div>404</div>
-    )
+	return (
+		<>
+		<Header/>
+		<div id="main">
+			<div className="container">
+				<div className="wrap-404">
+					<div className="content-404">
+						<h1>Oops!</h1>
+						<p>We can't seem to find the page you're looking for.</p>
+					</div>
+					<div className="img">
+						<img src={img404} alt="404, No Path found for this page" />
+					</div>
+				</div>
+			</div>
+		</div>
+        <Footer/>
+		</>
+	)
 }
 
 export default NotFound404;
