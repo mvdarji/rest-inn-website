e18fff76bf6683d60e877c4e4c115e8c2d222ea1
Manali Darji <mvdarji@seneca.ca>
2022-04-19 22:58:40 -0400

    implemented login functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,35 +1,35 @@
 import {Link, useNavigate} from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const LoginForm = () => {
 	const navigate = useNavigate();
 	// all fields state varibales and respective error variables
-	const [emailId, setEmailId] = useState('');
-	const [errorEmailId, setErrorEmailId] = useState('');
+	const [email, setemail] = useState('');
+	const [erroremail, setErroremail] = useState('');
 
 	const [password, setPassword] = useState('');
 	const [errorPassword, setErrorPassword] = useState('');
 
-	const loginUser = (e) => {
-		e.preventDefault();
-
-		if(validation()){
-			navigate('/', { replace: true});
+	useEffect(() => {
+		const user = JSON.parse(localStorage.getItem('user'));		
+		if(user !== null){
+			navigate('/welcome', { replace: true});
 		}
-	}
+	});
+
 
 	const validation = () => {
 		const validEmailFormat = /^[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+(?:@[A-Za-z0-9-]+)(?:.[A-Za-z0-9-]+)$/;
 		let isValidated = true;
 
-		if( emailId === "" ){
-			setErrorEmailId("Email-ID can't be empty");
+		if( email === "" ){
+			setErroremail("Email-ID can't be empty");
 			isValidated = false;
-		}else if( !emailId.match(validEmailFormat) ){
-			setErrorEmailId("Email-ID format is incorrect");
+		}else if( !email.match(validEmailFormat) ){
+			setErroremail("Email-ID format is incorrect");
 			isValidated = false;
 		}else{
-			setErrorEmailId("");
+			setErroremail("");
 		}
 
 		if( password === "" ){
@@ -42,16 +42,57 @@ const LoginForm = () => {
 		return isValidated;
 	}
 
+	const loginUser = (e) => {
+		e.preventDefault();
+
+		if( !document.getElementById('login-error-msg').classList.contains('hide') ){
+			document.getElementById('login-error-msg').classList.add('hide');
+		}
+
+		if(validation()){
+			const user = {
+				email,
+				password
+			};
+
+			fetch(`${process.env.REACT_APP_API_URI}/auth`, {
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				  },
+				method: 'POST',
+				body: JSON.stringify(user)
+			})
+			.then(resp => resp.json())
+			.then(loggedInUser => {
+				if(loggedInUser.id != null){
+					delete loggedInUser.password;
+					localStorage.setItem('user',JSON.stringify(loggedInUser));
+					navigate('/dashboard', { replace: true});
+				}else{
+					document.getElementById('login-error-msg').classList.remove('hide');
+				}
+			})
+			.catch(error => { 
+				console.log(error);
+				document.getElementById('login-error-msg').classList.remove('hide');
+			})
+		}
+	}	
+
 	return (
 		<div className="container divider">
 			<h5 className="section-title">Login to book a vacation!</h5>
 
 			<div className="form-wrap">
+				<div id='login-error-msg' className="hide text-center">
+					Email and password are incorrect. Please try logging in with correct credentials.
+				</div>
 				<form>
 					<div className="input-group">
 						<label htmlFor="email">E-mail ID *</label>
-						<input type="email" id="email" value={emailId} onChange={ (e) => { setEmailId(e.target.value) }}/>
-						<span className="error-msg">{errorEmailId}</span>
+						<input type="email" id="email" value={email} onChange={ (e) => { setemail(e.target.value) }}/>
+						<span className="error-msg">{erroremail}</span>
 					</div>
 
 					<div className="input-group">
