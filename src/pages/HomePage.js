f83ec9d6df699aa1f9b74ebf5b098b22b08b6b4a
Manali Darji <mvdarji@seneca.ca>
2022-04-19 23:04:00 -0400

    removed unwanted comment

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,9 +4,6 @@ import PropertyTypeLists from '../components/PropertyTypeLists';
 import BestSellerLists from '../components/BestSellerLists';
 import Footer from '../components/Footer';
 
-// in the end replace all api baseURL with 
-// https://rest-inn-api-mvdarji.herokuapp.com/
-
 const HomePage = () => {
 	return (
 		<>
